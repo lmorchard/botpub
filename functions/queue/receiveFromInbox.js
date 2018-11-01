1d672f9f9af46a9bb298a8c9850b3e984d6c8822
Les Orchard <me@lmorchard.com>
2018-10-31 22:39:13 -0400

    Move common note functions to lib/activities

--- functions/queue/receiveFromInbox.js
+++ functions/queue/receiveFromInbox.js
@@ -1,9 +1,8 @@
 const AWS = require("aws-sdk");
 
-const { createNote } = require("../../lib/activities");
-const { dateNow, uid, withContext } = require("../../lib/utils");
+const { sendNote } = require("../../lib/activities");
 const { fetchJson } = require("../../lib/request");
-const html = require("../../lib/html");
+const { withContext } = require("../../lib/utils");
 
 module.exports = async ({ body, config }) => {
   const { log, bots, SITE_URL } = config;
@@ -127,118 +126,4 @@ async function deleteFollower({ config, bot, actorDeref }) {
   log.debug("unfollowDelete", { deleteResult });
 
   await enqueue.updateSharedInboxes({ config });
-}
-
-// TODO: Split sendNote and publishNote out into separate reusable functions
-
-async function sendNote({
-  config,
-  bot,
-  actor,
-  actorDeref,
-  inReplyTo,
-  content,
-}) {
-  const S3 = new AWS.S3({ apiVersion: "2006-03-01" });
-  const { enqueue } = require("./index");
-  const { log, SITE_URL: siteURL, STATIC_BUCKET: Bucket } = config;
-  const {
-    profile: { name },
-  } = bot;
-  const objectUuid = uid();
-  const activity = createNote({
-    bot,
-    objectUuid,
-    actorURL: bot.id,
-    siteURL,
-    config,
-    actor,
-    actorDeref,
-    content,
-    inReplyTo,
-    published: dateNow(),
-  });
-  try {
-    await publishActivity({
-      bot,
-      activity,
-      objectUuid,
-      config,
-      actor,
-      actorDeref,
-      inReplyTo: activity.object.url,
-      content,
-    });
-  } catch (error) {
-    log.error("publishActivityError", { error: error.toString() });
-    return;
-  }
-  try {
-    await enqueue.deliverToRemoteInbox({
-      config,
-      body: { name, inbox: actorDeref.inbox, activity },
-    });
-  } catch (error) {
-    log.error("inboxDeliveryFailure", { error: error.toString() });
-  }
-  try {
-    const sharedInboxes = await S3.getObject({
-      Bucket,
-      Key: "sharedInboxes.json",
-    })
-      .promise()
-      .then(result => JSON.parse(result.Body.toString("utf-8")));
-    for (let inbox of sharedInboxes) {
-      await enqueue.deliverToRemoteInbox({
-        config,
-        body: { name, inbox, activity },
-      });
-    }
-  } catch (error) {
-    log.error("sharedInboxDeliveryFailure", { error: error.toString() });
-  }
-}
-
-async function publishActivity({
-  bot,
-  activity,
-  objectUuid,
-  config,
-  actor,
-  actorDeref,
-  content,
-  inReplyTo,
-}) {
-  const S3 = new AWS.S3({ apiVersion: "2006-03-01" });
-  const { log, STATIC_BUCKET: Bucket } = config;
-  const {
-    profile: { name },
-  } = bot;
-  const putResult = await Promise.all([
-    S3.putObject({
-      Bucket,
-      Key: `${name}/objects/Note/${objectUuid}.json`,
-      ContentType: "application/activity+json; charset=utf-8",
-      Body: JSON.stringify(withContext(activity.object), null, "  "),
-    }).promise(),
-    S3.putObject({
-      Bucket,
-      Key: `${name}/objects/Note/${objectUuid}.html`,
-      ContentType: "text/html; charset=utf-8",
-      Body: await html.object(activity.object),
-    }).promise(),
-    S3.putObject({
-      Bucket,
-      Key: `${name}/objects/Create/${objectUuid}.json`,
-      ContentType: "application/activity+json; charset=utf-8",
-      Body: JSON.stringify(withContext(activity), null, "  "),
-    }).promise(),
-    S3.putObject({
-      Bucket,
-      Key: `${name}/objects/Create/${objectUuid}.html`,
-      ContentType: "text/html; charset=utf-8",
-      Body: await html.object(activity),
-    }).promise(),
-  ]);
-  log.debug("putCreateNoteActivity", { putResult });
 }
